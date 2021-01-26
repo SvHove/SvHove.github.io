f35eae1f60f9e10c3813da5681825b185dfd18bd
SvHove <simon.vanhove@gmx.de>
2021-01-26 20:35:49 +0100

    1.02

--- scripts/swInclude.js
+++ scripts/swInclude.js
@@ -3,7 +3,13 @@
 if('serviceWorker' in navigator) {
     console.log('Installation started - main')
     navigator.serviceWorker.register('./sw.js')
-        .then(reg => console.log(reg))
-        .catch(err => console.log(err));
+        .then(reg => {
+            console.log(reg);
+            console.log('Service Worker registered!')
+        })
+        .catch(err => {
+            console.log(err);
+            console.log('Service Worker registration encountered an error.');
+        });
 }
 
--- sw.js
+++ sw.js
@@ -1,52 +1,78 @@
 console.log('Service worker reached...');
 
-let cacheName = 'pwa5';
+console.log('Second message');
 
-caches.open(cacheName).then(cache => {});
+let cacheName = 'pwa6';
 
-console.log('Cache opened' + cacheName);
+console.log('Variable created: ' + cacheName);
 
-self.addEventListener('install', event => {
-    console.log('Installing');
-    event.waitUntil(
-        caches.open(cacheName)
-            .then(cache => {
-                console.log('Started');
-                return cache.addAll(['./', './index.html', './manifest.webmanifest', './scripts/swInclude.js', './scripts/main.js', './styles/main.css',
-                    './res/backButton.svg', './res/homeButton.svg', './res/menuButton.svg', './res/searchButton.svg', './images/logo_kliniken_192.png',
-                    './images/logo_kliniken_512.png', './html/ab_index.html', './html/Allgemeines.html', './html/Chi1.html', './html/Chi2.html', './html/Chi3.html',
-                    './html/Chi4.html', './html/Gastro.html', './html/Gyn.html', './html/HNO.html', './html/Neuro1.html', './html/Neuro2.html', './html/Neuro3.html',
-                    './html/Neuro4.html', './html/PeriOP.html', './html/Pn1.html', './html/Pn2.html', './html/Pn3.html', './html/Pn4.html', './html/Präambel.html',
-                    './html/Sepsis.html', './html/Uro.html']);
-            }).then(() => {
-            console.log('Installed, skipping waiting');
-            self.skipWaiting().then(() => {
-                console.log('Installation finished');
-            });
-        })
-    );
-});
+try {
+    caches.delete('pwa1');
+    caches.delete('pwa2');
+    caches.delete('pwa3');
+    caches.delete('pwa4');
+    caches.delete('pwa5');
+} catch(error) {
+    console.log(error);
+}
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.open(cacheName)
-            .then(cache => cache.match(event.request))
-            .then(response => response || fetch(event.request))
-    );
-})
+console.log('caches deleted?');
 
-self.addEventListener('activate', (e) => {
+try {
+    console.log('Creating Cache');
+    caches.open('pwa6').then(cache => {
+    });
+} catch(error) {
+    console.log('Cache could not be opened');
+}
 
-    e.waitUntil(
+console.log('Cache opened: ' + cacheName);
 
-        CacheStorage.keys().then(keys => {
-            for(let i=0;i<keys.length;i++) {
-                if(keys[i] !== cacheName) {
-                    CacheStorage.delete(keys[i]);
-                }
-            }
-        }));
-})
+try {
+    console.log('Adding install event listener.')
+    self.addEventListener('install', event => {
+        console.log('Installing');
+        event.waitUntil(
+            caches.open('pwa6')
+                .then(cache => {
+                    console.log('Started');
+                    return cache.addAll(['./', './index.html', './manifest.webmanifest', './scripts/swInclude.js', './scripts/main.js', './styles/main.css',
+                        './res/backButton.svg', './res/homeButton.svg', './res/menuButton.svg', './res/searchButton.svg', './images/logo_kliniken_192.png',
+                        './images/logo_kliniken_512.png', './html/ab_index.html', './html/Allgemeines.html', './html/Chi1.html', './html/Chi2.html', './html/Chi3.html',
+                        './html/Chi4.html', './html/Gastro.html', './html/Gyn.html', './html/HNO.html', './html/Neuro1.html', './html/Neuro2.html', './html/Neuro3.html',
+                        './html/Neuro4.html', './html/PeriOP.html', './html/Pn1.html', './html/Pn2.html', './html/Pn3.html', './html/Pn4.html', './html/Präambel.html',
+                        './html/Sepsis.html', './html/Uro.html']);
+                }).then(() => {
+                console.log('Installed, skipping waiting');
+                self.skipWaiting().then(() => {
+                    console.log('Installation finished');
+                });
+            })
+        );
+    });
+} catch(error) {
+    console.log('Install listener failed.');
+    console.log(error);
+}
+
+
+try {
+    console.log('Adding fetch listener');
+    self.addEventListener('fetch', event => {
+        console.log('Fetching now..');
+        event.respondWith(
+            caches.open('pwa6')
+                .then(cache => cache.match(event.request))
+                .then(response => response || fetch(event.request))
+        );
+        console.log('Fetch reached end');
+    });
+    console.log('Fetch listener added.');
+} catch(error) {
+    console.log('Fetching listener failed.');
+    console.log(error);
+}
+
 
 
 /*
