e60c66de725172e0f61c8b9b740c44f987c1ae26
SvHove <simon.vanhove@gmx.de>
2021-01-28 20:53:58 +0100

    Version 0.2

--- sw.js
+++ sw.js
@@ -1,35 +1,25 @@
-console.log('Service worker 1.13 reached...');
-
-console.log('Second message');
+let versionNumber = '0.2';
 
 let cacheName = 'pwa1';
 
-console.log('Cache opened: ' + cacheName);
-
 try {
-    console.log('Adding install event listener.')
     self.addEventListener('install', event => {
-        console.log('Installing');
         event.waitUntil(
             caches.open(cacheName)
                 .then(cache => {
-                    console.log('Starting to add to cache.');
                     return cache.addAll(['./', './index.html', './manifest.webmanifest', './scripts/swInclude.js', './scripts/main.js', './styles/main.css',
                         './res/backButton.svg', './res/homeButton.svg', './res/menuButton.svg', './res/searchButton.svg', './images/logo_kliniken_192.png',
                         './images/logo_kliniken_512.png', './html/ab_index.html', './html/Allgemeines.html', './html/Chi1.html', './html/Chi2.html', './html/Chi3.html',
                         './html/Chi4.html', './html/Gastro.html', './html/Gyn.html', './html/HNO.html', './html/Neuro1.html', './html/Neuro2.html', './html/Neuro3.html',
-                        './html/Neuro4.html', './html/PeriOP.html', './html/Pn1.html', './html/Pn2.html', './html/Pn3.html', './html/Pn4.html', './html/Präambel.html',
+                        './html/Neuro4.html', './html/PeriOP.html', './html/Pn1.html', './html/Pn2.html', './html/Pn3.html', './html/Pn4.html', './html/prae.html',
                         './html/Sepsis.html', './html/Uro.html']);
                 }).then(() => {
-                console.log('Installed, skipping waiting');
                 self.skipWaiting().then(() => {
-                    console.log('Installation of SW 1.11 finished');
                 });
             })
         );
     });
 } catch(error) {
-    console.log('Install listener failed.');
     console.log(error);
 }
 
