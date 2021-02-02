30171b1c055068644bacd908141c3dba4df7596a
SvHove <simon.vanhove@gmx.de>
2021-02-02 20:14:40 +0100

    Version 0.33

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-let versionNumber = '0.32';
+let versionNumber = '0.33';
 
 let cacheName = 'pwa2';
 
@@ -7,12 +7,12 @@ try {
         event.waitUntil(
             caches.open(cacheName)
                 .then(cache => {
-                    return cache.addAll(['./', './index.html', './changelog.html', './static.js', './manifest.webmanifest', './scripts/swInclude.js', './scripts/main.js', './styles/main.css',
+                    return cache.addAll(['./', './index.html', './changelog.html', './manifest.webmanifest', './scripts/swInclude.js', './scripts/main.js', './styles/main.css',
                         './res/backButton.svg', './res/homeButton.svg', './res/menuButton.svg', './res/searchButton.svg', './images/logo_kliniken_192.png',
                         './images/logo_kliniken_512.png', './apple-touch-icon-120x120.png', './apple-touch-icon-152x152.png', './apple-touch-icon-180x180.png', './AB_Guideline/ab_index.html', './AB_Guideline/Allgemeines.html', './AB_Guideline/Chi1.html', './AB_Guideline/Chi2.html', './AB_Guideline/Chi3.html',
                         './AB_Guideline/Chi4.html', './AB_Guideline/Gastro.html', './AB_Guideline/Gyn.html', './AB_Guideline/HNO.html', './AB_Guideline/Neuro1.html', './AB_Guideline/Neuro2.html', './AB_Guideline/Neuro3.html',
                         './AB_Guideline/Neuro4.html', './AB_Guideline/PeriOP.html', './AB_Guideline/Pn1.html', './AB_Guideline/Pn2.html', './AB_Guideline/Pn3.html', './AB_Guideline/Pn4.html', './AB_Guideline/prae.html',
-                        './AB_Guideline/Sepsis.html', './AB_Guideline/Uro.html', './AB_Guideline/static.js', './SOP/sop_index.html', './SOP/AE_Pneumonie.html', './SOP/COPD.html', './SOP/LAE.html', './SOP/static.js']);
+                        './AB_Guideline/Sepsis.html', './AB_Guideline/Uro.html', './SOP/sop_index.html', './SOP/AE_Pneumonie.html', './SOP/COPD.html', './SOP/LAE.html']);
                 }).then(() => {
                 self.skipWaiting().then(() => {
                 });
