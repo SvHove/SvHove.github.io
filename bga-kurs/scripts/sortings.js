082f910e75f26e290649d3c49dfb01863f8f288a
SvHove <simon.vanhove@gmx.de>
2024-05-25 12:21:44 +0200

    Image test

---
+++ bga-kurs/scripts/sortings.js
@@ -0,0 +1,7 @@
+let layoutEmden = ["pH", "pO2", "pCO2", "Hct", "Hb", "sO2", ]
+
+let layoutAurich = ["pH", ]
+
+let layoutOthers = ["Leuko", ]
+
+let layoutGeneral = ["pH", "pO2", "pCO2", "HCO3", "BE", "AL", "HcT", "Hb", "sO2", "Na", "K", "Glu", "Lac"]
